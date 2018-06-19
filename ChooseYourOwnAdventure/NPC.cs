679d88d2b08b883159f5f4d57bf4d8c9c004d5ae
buck-nicholas <buck.nicholas.c@gmail.com>
2018-06-19 15:48:32 -0500

    Added NPC Class

---
+++ ChooseYourOwnAdventure/NPC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourOwnAdventure
+{
+    class NPC
+    {
+        // Member Variables
+        public string name;
+        public bool aggro;
+        public double health;
+
+        // constructor
+        public NPC(string name, bool aggro, double health)
+        {
+            this.name = name;
+            this.aggro = aggro;
+            this.health = health;
+        }
+        // methods
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(name + ": " + message);
+        }
+    }
+}
